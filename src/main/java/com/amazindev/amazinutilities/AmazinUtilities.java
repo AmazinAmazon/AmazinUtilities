8db583ec330b6775bc5f051cebc2440a540d52a5
AmazinAmazon <mati.bertrand@icloud.com>
2021-10-29 11:17:53 +0200

    First Commit

---
+++ src/main/java/com/amazindev/amazinutilities/AmazinUtilities.java
@@ -0,0 +1,17 @@
+package com.amazindev.amazinutilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AmazinUtilities extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
